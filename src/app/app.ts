c2ffd50c9b1579db628b65fa8b68c15e5d5bb99b
mephisto-inc <230451722+mephisto-inc@users.noreply.github.com>
2026-09-16 17:36:09 -0300

    Getting started with Angular - Learning Angular (Part 2)

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  imports: [RouterOutlet],
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('Learning-Angular, and its universe! Angular is a powerful framework for building web applications.');
+}
