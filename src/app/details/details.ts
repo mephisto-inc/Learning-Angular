0e0e1c40fdf6a005bcb4795b8601a5154f93f2d9
mephisto-inc <230451722+mephisto-inc@users.noreply.github.com>
2026-09-17 16:17:25 -0300

    Forms in Angular - Learning Angular (Part 7)

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -3,41 +3,66 @@ import {ActivatedRoute} from '@angular/router';
 import {inject} from '@angular/core';
 import {Housing} from '../housing';
 import {HousingLocationInterface} from '../housing-location-interface';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   selector: 'app-details',
   styleUrl: './details.css',
   template: `
-  <article>
-    <img class="listing-photo" [src]="housingLocationInterface?.photo" alt="Photo of {{ housingLocationInterface?.name }}">
-    <section class="listing-description">
-      <h2 class="listing-heading">{{ housingLocationInterface?.name }}</h2>
-      <p class="listing-location">{{ housingLocationInterface?.city }}, {{ housingLocationInterface?.state }}</p>
-    </section>
-    <section class="listing-features">
-      <h2 class="section-heading">About this housing location</h2>
-      <ul>
-        <li>Units available: {{ housingLocationInterface?.availableUnits }}</li>
-        <li>Does this location have wifi: {{ housingLocationInterface?.wifi }}</li>
-        <li>Does this location have laundry: {{ housingLocationInterface?.laundry }}</li>
-      </ul>
-    </section>
-    <section class="listing-apply">
-      <h2 class="section-heading">Apply now to live here</h2>
-      <button class="primary" type="button">Apply now</button>
-    </section>
-  </article>
+    <article>
+      <img class="listing-photo" [src]="housingLocationInterface?.photo"
+           alt="Photo of {{ housingLocationInterface?.name }}">
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocationInterface?.name }}</h2>
+        <p class="listing-location">{{ housingLocationInterface?.city }}, {{ housingLocationInterface?.state }}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{ housingLocationInterface?.availableUnits }}</li>
+          <li>Does this location have wifi: {{ housingLocationInterface?.wifi }}</li>
+          <li>Does this location have laundry: {{ housingLocationInterface?.laundry }}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email">
+          <button type="submit" class="primary">Apply now</button>
+        </form>
+      </section>
+    </article>
   `,
 })
 class Details {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService: Housing = inject(Housing);
   housingLocationInterface: HousingLocationInterface | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
     this.housingLocationInterface = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '');
   }
 }
 
--- src/app/housing.ts
+++ src/app/housing.ts
@@ -112,4 +112,8 @@ export class Housing {
   getHousingLocationById(id: number): HousingLocationInterface | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string): void {
+    console.log('Application received: ', firstName, lastName, email);
+  }
 }
